63cda1109f177b97c1ec3cb6632b61b61b38833a
yashwanthvarmav <yashwanthvarmav@gmail.com>
2024-04-03 15:30:42 +0100

    Implemented login and logout

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, g, jsonify
 from . import db
-from flask import request
+from flask import request, session, redirect, url_for
 import secrets
 import json
 import string
@@ -16,21 +16,47 @@ def create_app():
     app.config['SECRET_KEY'] = generate_secret_key()
 
     app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'dbs'
-    app.config['MYSQL_PASSWORD'] = 'password'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = 'yash@1999'
     app.config['MYSQL_DB'] = 'DIMS'
 
     # Initialize the database
     #db.init_app(app)
 
-    @app.route('/')
+    @app.route('/', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+
+            db = get_db()
+            cursor = db.cursor()
+            cursor.execute('SELECT * FROM USER WHERE username=%s AND password=%s', (username, password))
+            record = cursor.fetchone()
+            cursor.close()
+
+            if record:
+                session['loggedin'] = True
+                session['username'] = username
+                return redirect(url_for('index'))
+            else:
+                msg = 'Incorrect Username or Password'
+                return render_template('login.html', msg=msg)
+        else:
+            return render_template('login.html')
+
+
+    @app.route('/index')
     def index():
-        db = get_db()
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM Scanners")
-        data = cursor.fetchall()
-        cursor.close()
-        return render_template('index.html', data=data)
+        if 'loggedin' in session:
+            db = get_db()
+            cursor = db.cursor()
+            cursor.execute("SELECT * FROM Scanners")
+            data = cursor.fetchall()
+            cursor.close()
+            return render_template('index.html', data=data, username=session['username'])
+        else:
+            return redirect(url_for('login'))
         #return data
     
     @app.route('/manage-devices')
@@ -48,6 +74,10 @@ def create_app():
     @app.route('/settings')
     def settings():
         return render_template('settings.html')
+
+    @app.route('/logout')
+    def logout():
+        return render_template('login.html')
     
     @app.route('/api/data')
     def get_data():
@@ -132,60 +162,30 @@ def create_app():
         except Exception as e:
             return jsonify({"error": "Failed to delete Device record", "details": str(e)}), 500
 
-
     '''@app.route('/insert', methods=['POST'])
     def insert():
         db = get_db()
         cursor = db.cursor()
         # Retrieve data from the request and insert into the database
         cursor.close()
-        
-        if record:
-            session['loggedin'] = True
-            session['username'] = username
-            return redirect(url_for('index'))
-        else:
-            msg = 'Incorrect Password or username.'
-            return render_template('login.html', msg=msg)
-    else:
-        return render_template("login.html")
-    
-@app.route('/index', methods=['GET'])
-def index():
-    if 'loggedin' in session:
-        return render_template('index.html', username=session['username'])
-    else:
-        return redirect(url_for('login'))
+        db.commit()
+        return redirect(url_for('index'))'''
 
-@app.route('/manage-devices')
-def manageDevices():
-    device_data = [
-        {'name': 'Device 1', 'type': 'Type 1'},
-        {'name': 'Device 2', 'type': 'Type 2'},
-    ]
-    return render_template('manage-devices.html')
+    '''@app.route('/select')
+    def select():
+        db = get_db()
+        cursor = db.cursor()
+        # Execute select query and fetch data
+        cursor.execute("SELECT * FROM test")
+        data = cursor.fetchall()
+        cursor.close()
+        return render_template('select.html', data=data)'''
 
-@app.route('/manage-users')
-def manageUsers():
-    return render_template('manage-users.html')
+    @app.teardown_appcontext
+    def close_db(error):
+        db.close_db()
 
-@app.route('/generate-reports')
-def generateReports():
-    return render_template('generate-reports.html')
-
-@app.route('/settings')
-def settings():
-    return render_template('settings.html')
-
-@app.route('/logout')
-def logout():
-    return render_template('login.html')
-
-
-@app.route('/logout')
-def logout():
-    return render_template('login.html')
-
+    return app
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port='8080')
