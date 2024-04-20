101cf370d8a58442b8a2fc0ee78ac1531c3af8c6
Nishshanka Sirisena <nishshanka123@gmail.com>
2024-04-20 14:21:48 +0100

    mysql query updates for modifying data

--- flaskr/mysql-query.sql
+++ flaskr/mysql-query.sql
@@ -297,4 +297,139 @@ SELECT tp.SerialNo,
     tp.Description 
     FROM ThirdpartyDevice as tp, Device as d 
     WHERE tp.AssetNo = d.AssetNo;
+
+
+CREATE DEFINER=`nishshanka`@`localhost` PROCEDURE `GetAllDevices`(
+in dev_type VARCHAR(20), 
+in dev_name VARCHAR(20), 
+in employee_id INT, 
+in department_id INT
+)
+BEGIN
+	DROP TABLE IF EXISTS device_info;
+	create temporary table device_info (
+			`SerialNo` VARCHAR(20),
+			`OSFW` VARCHAR(20),
+			`MorM` VARCHAR(20),
+			`PMDate` DATE,
+			`AssetNo` VARCHAR(12),
+			`Named` VARCHAR(20),
+			`Condition` VARCHAR(20),
+			`device_type` VARCHAR(50),
+			`Description` VARCHAR(40),
+            `EmployeeID` INT,
+            `ProjectID` INT
+		);
+	-- extract all device data from relavant tables.
+    insert into device_info
+		SELECT cm.SerialNo, 
+			cm.FirmwareVersion, 
+			cm.ModelNumber, 
+			cm.ManufactureDate,
+			d.assetNo, 
+			d.device_name, 
+			d.device_condition, 
+			d.device_type,
+			"N A",
+			cm.EmployeeID,
+			cm.ProjectID
+		FROM CompanyManufacturedDevice as cm, 
+		Device as d where cm.AssetNo = d.AssetNo;
+		insert into device_info
+		SELECT tp.SerialNo, 
+		tp.OS, 
+		tp.Manufacturer, 
+		tp.PurchasedDate, 
+		d.AssetNo,
+		d.device_name, 
+		d.device_condition, 
+		d.device_type, 
+		tp.Description,
+        tp.EmployeeID,
+        tp.ProjectID
+		FROM ThirdpartyDevice as tp, Device as d 
+		WHERE tp.AssetNo = d.AssetNo;
+        
+	select * from device_info;
+END
     
+if dev_type = 'all' and dev_name = 'all'  then
+  begin
+		if employee_id = 0 and department_id = 0 then
+			begin
+				select * from device_info;
+			end;
+		elseif employee_id != 0 and department_id = 0 then
+			begin
+				select * from device_info;
+			end;
+		elseif employee_id = 0 and department_id != 0 then
+			begin
+				select * from device_info;
+			end;
+		elseif employee_id != 0 and department_id != 0 then
+			begin
+				select * from device_info;
+			end;
+		end if;
+  end;
+elseif dev_type != 'all' and dev_name = 'all'  then
+  begin
+		if employee_id = 0 and department_id = 0 then
+			begin
+				select * from device_info;
+			end;
+        elseif employee_id != 0 and department_id = 0 then
+			begin
+				select * from device_info;
+			end;
+        elseif employee_id = 0 and department_id != 0 then
+			begin
+				select * from device_info;
+			end;
+        elseif employee_id != 0 and department_id != 0 then  
+			begin
+				select * from device_info;
+			end;
+  end;
+elseif dev_type = 'all' and dev_name != 'all'  then
+  begin
+		if employee_id = 0 and department_id = 0 then
+			begin
+				select * from device_info;
+			end;
+        elseif employee_id != 0 and department_id = 0 then
+			begin
+				select * from device_info;
+			end;
+        elseif employee_id = 0 and department_id != 0 then
+			begin
+				select * from device_info;
+			end;
+        elseif employee_id != 0 and department_id != 0 then  
+			begin
+				select * from device_info;
+			end;
+		end if;
+  end;
+elseif dev_type = 'all' and dev_name = 'all'  then
+  begin
+		if employee_id = 0 and department_id = 0 then
+			begin
+				select * from device_info;
+			end;
+        elseif employee_id != 0 and department_id = 0 then
+			begin
+				select * from device_info;
+			end;
+        elseif employee_id = 0 and department_id != 0 then
+			begin
+				select * from device_info;
+			end;
+        elseif employee_id != 0 and department_id != 0 then
+			begin
+				select * from device_info;
+			end;
+		end if;
+  end;
+end if;
